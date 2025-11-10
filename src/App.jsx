bd825e010e1f9462ebf285f917a16cc597270253
stefanobellomo <stefanobellomo1998@gmail.com>
2025-11-10 17:48:34 +0100

    create new main component and add his code

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import Main from "./components/Main"
 
 function App() {
 
@@ -15,58 +15,63 @@ function App() {
     { "id": 10, "title": "Note di Changelog" }
   ]
 
-  const [articles, setArticles] = useState(titleArticles)
-  const [newArticle, setNewArticle] = useState('')
+  // const [articles, setArticles] = useState(titleArticles)
+  // const [newArticle, setNewArticle] = useState('')
 
-  function handleSubmit(e) {
-    e.preventDefault();
+  // function handleSubmit(e) {
+  //   e.preventDefault();
 
-    if (newArticle.length < 4) {
-      setNewArticle('')
-    } else {
-      const article = {
-        id: articles[articles.length - 1].id + 1,
-        title: newArticle,
-      }
-      const addNewArticle = [...articles, article]
-      setArticles(addNewArticle)
-      setNewArticle('')
-    }
-  }
+  //   if (newArticle.length < 4) {
+  //     setNewArticle('')
+  //   } else {
+  //     const article = {
+  //       id: articles[articles.length - 1].id + 1,
+  //       title: newArticle,
+  //     }
+  //     const addNewArticle = [...articles, article]
+  //     setArticles(addNewArticle)
+  //     setNewArticle('')
+  //   }
+  // }
 
-  function handleTrash(id) {
-    const filteredArticles = articles.filter((article) => article.id !== id)
-    setArticles(filteredArticles)
-  }
+  // function handleTrash(id) {
+  //   const filteredArticles = articles.filter((article) => article.id !== id)
+  //   setArticles(filteredArticles)
+  // }
 
   return (
     <>
 
-      <main className="container">
+      <Main titleArticles={titleArticles} />
 
-        <h1>Write your article</h1>
+      {/* <main>
 
-        <div className="col-6 mx-auto">
-          <div className="card p-3">
+        <div className="container">
+          <h1>Write your article</h1>
 
-            <form onSubmit={handleSubmit}>
-              <input type="text" value={newArticle} onChange={(e) => setNewArticle(e.target.value)} />
-              <button className="badge bg-success my-4 mx-4" type="submit">Add</button>
-            </form>
+          <div className="col-6 mx-auto">
+            <div className="card p-3">
 
-            <ul className="list-group">
-              {articles.map(article => (
-                <li key={article.id}
-                  className="list-group-item d-flex justify-content-between align-items-center">
-                  {article.title}
-                  <button className="badge bg-danger badge-pill" onClick={() => handleTrash(article.id)}><i className="bi bi-trash"></i></button>
-                </li>
-              ))}
-            </ul>
+              <form onSubmit={handleSubmit}>
+                <input type="text" value={newArticle} onChange={(e) => setNewArticle(e.target.value)} />
+                <button className="badge bg-success my-4 mx-4" type="submit">Add</button>
+              </form>
 
+              <ul className="list-group">
+                {articles.map(article => (
+                  <li key={article.id}
+                    className="list-group-item d-flex justify-content-between align-items-center">
+                    {article.title}
+                    <button className="badge bg-danger badge-pill" onClick={() => handleTrash(article.id)}><i className="bi bi-trash"></i></button>
+                  </li>
+                ))}
+              </ul>
+
+            </div>
           </div>
         </div>
-      </main>
+
+      </main> */}
 
 
 
